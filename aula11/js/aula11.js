7d6d70d6b1b9ce2b7e442f9edc8b1d4243a0998a
mykoll007 <mykoll.daniel@gmail.com>
2024-03-14 21:54:00 -0300

    aula 11/ correção aula 06

---
+++ aula11/js/aula11.js
@@ -0,0 +1,34 @@
+/*    Objetos
+let nome_var = {
+    atributo: característica
+    atributo: característica
+    atributo: característica}
+    */ 
+let alunos = [
+    {
+        ra:1010,
+        nome:"Fulano",
+        idade: 20
+
+    },
+    {
+        nome: "Fulana",
+        idade: 21,
+        ra:1011
+    }
+]
+
+// ela ler atraves da posição do array e puxando o atributo com ponto Ex: console.log(alunos[0].ra) 
+
+alunos.map(aluno =>{
+    console.log(aluno.ra)
+    console.log(aluno.nome)
+    console.log(aluno.idade)
+})
+
+let alunos1 =[1010,'Fulano', 20, 'Fulana', 21, 1011]
+
+z
+
+
+
